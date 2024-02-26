6669350ff579df2dc4876cdbbbf0fa949258ff4c
Inez Carrillo <inezcarrillo@Inezs-MacBook-Pro.local>
2024-02-25 21:12:14 -0600

    created db and tables

---
+++ db/schema.sql
@@ -0,0 +1,30 @@
+DROP DATABASE IF EXISTS employee_db;
+
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+CREATE TABLE department (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(25) NOT NULL 
+);
+
+CREATE TABLE role (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(25) NOT NULL,
+    salary DECIMAL NOT NULL, 
+    department INT,
+    FOREIGN KEY (department)
+    REFERENCES department(id)
+    ON DELETE SET NULL 
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(25) NOT NULL,
+    last_name VARCHAR(25) NOT NULL,
+    role_id INT,
+    FOREIGN KEY (role)
+    REFERENCES role(id)
+    ON DELETE SET NULL 
+);
